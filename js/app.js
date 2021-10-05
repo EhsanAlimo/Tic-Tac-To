304e41fa1eac1cfffd54ab991e89ffa7b71072dd
EhsanAlimo <ehsan.russell69@gmail.com>
2021-10-05 14:36:24 +1100

    firt commit for tic toc

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const boxes = Array.from(document.getElementsByClassName("box"));
+// console.log(boxes);
+const playText = document.getElementById("playText");
+const restartBtn = document.getElementById("restartBtn");
+const spaces = [];
+const O_TEXT = "0";
+const X_TEXT = "x";
+let currentPlayer;
